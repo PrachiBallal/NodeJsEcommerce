ff062873425019578a9b5dd5876acd206477b64d
Prachi <prachiballal893@gmail.com>
2022-01-28 13:11:24 +0530

    html page added to react. data linked dynamicaly

---
+++ frontend/src/data.js
@@ -0,0 +1,50 @@
+const data ={
+    product:[
+        {
+        _id:'1',
+        name:'Nykaa Royal Pink Dress',
+        category:'Dress',
+        image:'images/p1.jpg',
+        price: 2499,
+        brand:'Nykaa',
+        rating:3,
+        numReviews:10,
+        description:'high quality product',
+        },
+        {
+        _id:'2',
+        name:'Blue long Dress',
+        category:'Dress',
+        image:'images/p2.jpg',
+        price: 4499,
+        brand:'Zara',
+        rating:4,
+        numReviews:100,
+        description:'high quality product',
+        },
+        {
+        _id:'3',
+        name:'Pink Dress',
+        category:'Dress',
+        image:'images/p3.jpg',
+        price: 3499,
+        brand:'Nykaa',
+        rating:3.5,
+        numReviews:50,
+        description:'high quality product',
+        },
+        {
+        _id:'4',
+        name:'Long Formal Gown',
+        category:'Dress',
+        image:'images/p4.jpg',
+        price: 3499,
+        brand:'HnM',
+        rating:4.5,
+        numReviews:100,
+        description:'high quality product'
+        },       
+    ],
+};
+
+export default data;
